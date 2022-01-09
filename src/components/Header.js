c6ed5d15b9359d04db909c8f13335ecc9574296f
ozaratem <zaratemorera.of@gmail.com>
2022-01-09 16:50:04 -0600

    first commit :rocket:

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+    return <header className="Header">
+        <h1 className="title">Meteorite Landings</h1>
+        <p>Visualization tool to navigate a data set from The Meteoritical Society that contains information on all of the known meteorite landings </p>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Layout() {
+    return <div>
+        ldsjkfhnlksdfldsnfkljsd
+    </div>
+} 
+
+export default Layout;
