cfaf0d0fd3ac559913e911b2dcbfa6ec67869831
ozaratem <zaratemorera.of@gmail.com>
2022-01-12 07:21:28 -0600

    pass data to map and create markers

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Map/>
+      <Map data={cardsData} />
       <div className="cards-container">
         {
           cardsData.map(element => {
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
 
 
-function Map() {
+
+function Map(props) {
     return <div id="map">
-        <MapContainer center={[51.505, -0.09]} zoom={3} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+        <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
+            <TileLayer 
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            />
+            {
+                props.data.map(element => {
+                    return <CircleMarker key={element.id} center={[element.reclat, element.reclong]}> 
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </CircleMarker>
+                })
+            }
+        </MapContainer>
     </div>
 }
 
