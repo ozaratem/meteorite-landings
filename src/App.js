e60eafb20443326210ffbbb56f5db661f1093019
ozaratem <zaratemorera.of@gmail.com>
2022-01-16 17:48:08 -0600

    add scale and format circle markers in map

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Card from "./components/Card";
 import meteoriteData from "./data/Meteorite_Landings_dataset.json";
 import Map from "./components/Map";
 import './App.css';
+import {min, max} from "d3-array";
+
+const minMass = min(meteoriteData, function(node){return +node.mass});
+const maxMass = max(meteoriteData, function(node){return +node.mass});
 
 function App() {
-  const cardsData = meteoriteData.slice(0,100);
+  const cardsData = meteoriteData.splice(0, 100);
+	const mapData = meteoriteData.splice(0, 1000);
   return (
     <div className="App">
       <Header/>
-      <Map data={cardsData} />
+      <Map data={mapData} minMax={[minMass, maxMass]} />
       <div className="cards-container">
         {
           cardsData.map(element => {
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,26 +1,46 @@
 import React from "react";
 import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
+import {scalePow} from "d3-scale";
 
-
+function getColor(itemFall) {
+	if (itemFall === "Fell") {
+		return "#FC8D62"
+	} if (itemFall === "Found") {
+		return "#8DA0CB"
+	}
+}
 
 function Map(props) {
-    return <div id="map">
-        <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
-            <TileLayer 
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-            />
-            {
-                props.data.map(element => {
-                    return <CircleMarker key={element.id} center={[element.reclat, element.reclong]}> 
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </CircleMarker>
-                })
-            }
-        </MapContainer>
-    </div>
+	const min = props.minMax[0];
+	const max = props.minMax[1];
+
+	const scale = scalePow().exponent(0.2).domain([min, max]).range([5, 100]);
+	
+	return <div id="map">
+    <MapContainer center={[16, 0]} zoom={2} scrollWheelZoom={false}>
+      <TileLayer 
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+      />
+      {
+        props.data.map(element => {
+        	return <CircleMarker 
+						key={element.id}
+						center={[element.reclat, element.reclong]}
+						radius={scale(element.mass)}
+						color={getColor(element.fall)}
+						weight={1}
+						fillOpacity={0.5}
+					> 
+            <Popup>
+              <h3>{element.name}</h3>
+							 <p>{element.mass} grams</p>
+            </Popup>
+          </CircleMarker>
+        })
+      }
+    </MapContainer>
+	</div>
 }
 
 export default Map;
