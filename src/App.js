774b4aff77036a43ae4a3afa6c0ed86c1cc71dd3
ozaratem <zaratemorera.of@gmail.com>
2022-01-18 07:26:02 -0600

    create pagination state

--- src/App.js
+++ src/App.js
@@ -4,20 +4,48 @@ import meteoriteData from "./data/Meteorite_Landings_dataset.json";
 import Map from "./components/Map";
 import './App.css';
 import {min, max} from "d3-array";
+import {useState} from "react";
 
 const minMass = min(meteoriteData, function(node){return +node.mass});
 const maxMass = max(meteoriteData, function(node){return +node.mass});
+const sortedData = meteoriteData.sort(function (a, b){
+  return parseInt(b.mass) - parseInt(a.mass);
+});
+
+const countElements = 100;
 
 function App() {
-	const sortedData = meteoriteData.sort(function (a, b){
-		return parseInt(b.mass) - parseInt(a.mass);
-	});
-  const cardsData = sortedData.slice(0, 100);
-	const mapData = sortedData.slice(0, 300);
+
+  const pageState = useState(0)
+  const page = pageState[0];
+  const setPage = pageState[1];
+  const start = page * countElements;
+  const end = (page * countElements) + countElements;
+
+  const cardsData = sortedData.slice(start, end);
+	const mapData = sortedData.slice(start, end);
+
+  function handleClickBack(e) {
+    if (page !== 0) {
+      const nextValue = page - 1;
+      setPage(nextValue);
+    }
+  }
+
+  function handleClickNext(e) {
+    // TODO: fix handle end of array 
+    const nextValue = page + 1;
+    setPage(nextValue);
+  }
+
   return (
     <div className="App">
       <Header/>
       <Map data={mapData} minMax={[mapData[mapData.length-1].mass, maxMass]} />
+      <div className="controls">
+        <div onClick={handleClickBack}>Back</div>
+        <div onClick={handleClickNext}>Next</div>
+      </div>
       <div className="cards-container">
         {
           cardsData.map(element => {
